9a2e04bb7977f01a286e886edc75d58fa9d69221
Roman Astashevich <irqmen@gmail.com>
2017-01-11 16:47:28 +0300

    workflow: add npm publish tasks

--- tools/gulp/tasks/release.ts
+++ tools/gulp/tasks/release.ts
@@ -1,11 +1,18 @@
+import {spawn} from 'child_process';
+import {existsSync, statSync} from 'fs';
 import {task} from 'gulp';
+import * as minimist from 'minimist';
+import * as path from 'path';
 import * as gulpRunSequence from 'run-sequence';
 
 import {execTask, cleanTask} from '../helpers';
-import {dirs, resolve} from '../settings/paths';
+import {DIST_COMPONENTS_ROOT, DIST_ROOT, resolve} from '../settings/paths';
 
 
-task(':build:release:clean-spec', cleanTask(resolve(dirs.dist, '**/*.spec.*')));
+const argv = minimist(process.argv.slice(3));
+
+
+task(':build:release:clean-spec', cleanTask(resolve(DIST_ROOT, '**/*.spec.*')));
 
 
 task('build:release', function(done: () => void) {
@@ -14,6 +21,85 @@ task('build:release', function(done: () => void) {
     'clean',
     ':build:components:ngc',
     ':build:release:clean-spec',
+    done
+  );
+});
+
+
+/** Make sure we're logged in. */
+task(':publish:whoami', execTask('npm', ['whoami'], {
+  silent: true,
+  errMessage: 'You must be logged in to publish.'
+}));
+
+task(':publish:logout', execTask('npm', ['logout']));
+
+
+function _execNpmPublish(label: string): Promise<{}> {
+  const packageDir = DIST_COMPONENTS_ROOT;
+  if (!statSync(packageDir).isDirectory()) {
+    return;
+  }
+
+  if (!existsSync(path.join(packageDir, 'package.json'))) {
+    throw new Error(`"${packageDir}" does not have a package.json.`);
+  }
+
+  process.chdir(packageDir);
+  console.log(`Publishing material...`);
+
+  const command = 'npm';
+  const args = ['publish', '--access', 'public', label ? `--tag` : undefined, label || undefined];
+  return new Promise((resolve, reject) => {
+    console.log(`  Executing "${command} ${args.join(' ')}"...`);
+    if (argv['dry']) {
+      resolve();
+      return;
+    }
+
+    const childProcess = spawn(command, args);
+    childProcess.stdout.on('data', (data: Buffer) => {
+      console.log(`  stdout: ${data.toString().split(/[\n\r]/g).join('\n          ')}`);
+    });
+    childProcess.stderr.on('data', (data: Buffer) => {
+      console.error(`  stderr: ${data.toString().split(/[\n\r]/g).join('\n          ')}`);
+    });
+
+    childProcess.on('close', (code: number) => {
+      if (code == 0) {
+        resolve();
+      } else {
+        reject(new Error(`Project did not publish, status: ${code}.`));
+      }
+    });
+  });
+}
+
+task(':publish', function(done: (err?: any) => void) {
+  const label = argv['tag'];
+  const currentDir = process.cwd();
+
+  if (!label) {
+    console.log('You can use a label with --tag=labelName.');
+    console.log('Publishing using the latest tag.');
+  } else {
+    console.log(`Publishing using the ${label} tag.`);
+  }
+  console.log('\n\n');
+
+  // Publish only the material package.
+  return _execNpmPublish(label)
+    .then(() => done())
+    .catch((err: Error) => done(err))
+    .then(() => process.chdir(currentDir));
+});
+
+task('publish', function(done: () => void) {
+  gulpRunSequence(
+    ':publish:whoami',
+    'build:release',
+    ':publish',
+    ':publish:logout',
     done
   );
 });
